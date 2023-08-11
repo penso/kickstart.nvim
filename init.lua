ce5382e3133e2730e71ca3bb9d7773b8824736ee
Fabien Penso <git@pen.so>
2023-08-11 23:02:50 +0200

    Add ripgrep

--- init.lua
+++ init.lua
@@ -136,6 +136,7 @@ require('lazy').setup({
     branch = '0.1.x',
     dependencies = {
       'nvim-lua/plenary.nvim',
+      'BurntSushi/ripgrep',
       -- Fuzzy Finder Algorithm which requires local dependencies to be built.
       -- Only load if `make` is available. Make sure you have the system
       -- requirements installed.
