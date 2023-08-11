39067181f65a108bb10dc06bd3fdde9b80df861a
Fabien Penso <git@pen.so>
2023-08-11 23:45:16 +0200

    Add twilight

--- lua/custom/plugins/zen-mode.lua
+++ lua/custom/plugins/zen-mode.lua
@@ -4,5 +4,14 @@ return {
     -- your configuration comes here
     -- or leave it empty to use the default settings
     -- refer to the configuration section below
+    plugins = {
+      alacritty = {
+        enabled = true,
+        font = "24", -- font size
+      },
+      gitsigns = { enabled = true },
+      twilight = { enabled = true },
+      tmux = { enabled = true },
+    },
   }
 }
