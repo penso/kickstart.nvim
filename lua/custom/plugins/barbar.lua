539cd5f21b1a4553abf8309e06fa102591fd53c4
Fabien Penso <git@pen.so>
2023-08-11 22:25:00 +0200

    Hide file tree

--- lua/custom/plugins/barbar.lua
+++ lua/custom/plugins/barbar.lua
@@ -27,6 +27,9 @@ return {
     icons = {
       button = false,
     },
+    sidebar_filetypes = {
+      NvimTree = true,
+    },
   },
   version = '^1.0.0', -- optional: only update when a new 1.x version is released
 }
