1c7e866db56c7ae201f3993fb781ab8d16784f29
Fabien Penso <git@pen.so>
2023-08-13 12:55:45 +0200

    Add <leader>sr

--- init.lua
+++ init.lua
@@ -255,6 +255,7 @@ vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { de
 vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
 vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
 vim.keymap.set('n', '<leader>sc', require('telescope.builtin').colorscheme, { desc = '[S]earch [C]olorscheme' })
+vim.keymap.set('n', '<leader>sr', require('telescope.builtin').oldfiles, { desc = '[S]earch [R]ecent Files' })
 local wk = require("which-key")
 wk.register({
   s = {
