cce4625461ae98f47f125f0252c5f6f71c0e794b
Fabien Penso <git@pen.so>
2023-08-13 13:45:43 +0200

    Add noice

--- init.lua
+++ init.lua
@@ -224,7 +224,8 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 -- See `:help nvim-treesitter`
 require('nvim-treesitter.configs').setup {
   -- Add languages to be installed here that you want installed for treesitter
-  ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'typescript', 'vimdoc', 'vim' },
+  ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'typescript', 'vimdoc', 'vim', 'regex', 'lua',
+    'bash', 'markdown', 'markdown_inline' },
 
   -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
   auto_install = false,
--- lua/custom/plugins/notify.lua
+++ lua/custom/plugins/notify.lua
@@ -1,8 +1,19 @@
 return {
   'rcarriga/nvim-notify',
   config = function()
-    -- require('notify').setup {
-    -- }
+    require('notify').setup {
+      render = "minimal",
+    }
     require('telescope').load_extension('notify')
+
+    vim.notify = require('notify')
+
+    local wk = require("which-key")
+    wk.register({
+      s = {
+        name = "[S]earch",
+        n = { "<cmd>Telescope notify<cr>", "[N]notifications" },
+      },
+    }, { prefix = "<leader>" })
   end
 }
