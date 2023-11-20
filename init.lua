cdcc4a3ad4092480cd25180a5ae6705950b00ac3
Fabien Penso <git@pen.so>
2023-11-20 11:05:48 +0100

    Update plugins

--- init.lua
+++ init.lua
@@ -99,11 +99,8 @@ require('lazy').setup({
   {
     -- Add indentation guides even on blank lines
     'lukas-reineke/indent-blankline.nvim',
-    -- Enable `lukas-reineke/indent-blankline.nvim`
-    -- See `:help indent_blankline.txt`
+    main = "ibl",
     opts = {
-      char = '┊',
-      show_trailing_blankline_indent = false,
     },
   },
 
