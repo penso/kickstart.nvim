8b280e9b11dfafc439e5acb4e0e9094a4876df21
Fabien Penso <git@pen.so>
2024-01-12 17:26:05 +0100

    Update neovim

--- init.lua
+++ init.lua
@@ -65,6 +65,12 @@ require('lazy').setup({
   },
 
   {
+    "L3MON4D3/LuaSnip",
+    -- follow latest release.
+    version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+  },
+
+  {
     -- Autocompletion
     'hrsh7th/nvim-cmp',
     dependencies = {
