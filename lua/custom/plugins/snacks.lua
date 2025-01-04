5169b9a9bd75c7283363412c54a78a7e92e0db09
Fabien Penso <git@pen.so>
2025-01-04 15:06:29 +0100

    Setup snacks

--- lua/custom/plugins/snacks.lua
+++ lua/custom/plugins/snacks.lua
@@ -4,18 +4,10 @@ return {
   lazy = false,
   ---@type snacks.Config
   opts = {
-    -- your configuration comes here
-    -- or leave it empty to use the default settings
-    -- refer to the configuration section below
-    bigfile = { enabled = true },
-    dashboard = { enabled = true },
-    indent = { enabled = true },
-    input = { enabled = true },
     notifier = { enabled = true },
     quickfile = { enabled = true },
     scroll = { enabled = true },
-    statuscolumn = { enabled = true },
-    words = { enabled = true },
+    terminal = { enabled = true },
   },
   keys = {
     { "<leader>gg", function() Snacks.lazygit() end,     desc = "Lazygit" },
