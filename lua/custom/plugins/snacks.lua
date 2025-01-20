193ed23866b2dc5e067b3caaf4d161453c74817b
Fabien Penso <git@pen.so>
2025-01-20 19:24:46 +0100

    Add ctrl-/ for terminal

--- lua/custom/plugins/snacks.lua
+++ lua/custom/plugins/snacks.lua
@@ -1,21 +1,58 @@
 return {
-  "folke/snacks.nvim",
+  'folke/snacks.nvim',
   priority = 1000,
   lazy = false,
   ---@type snacks.Config
   opts = {
     notifier = { enabled = true },
     scroll = {
-      enabled = true,
+      enabled = false,
       animate = {
-        duration = { step = 5, total = 250 },
-        easing = "linear",
+        duration = { step = 20, total = 200 },
+        easing = 'linear',
       },
     },
     terminal = { enabled = true },
   },
   keys = {
-    { "<leader>gg", function() Snacks.lazygit() end,     desc = "Lazygit" },
-    { "<leader>gl", function() Snacks.lazygit.log() end, desc = "[G]it [L]og" },
-  }
+    {
+      '<leader>gg',
+      function()
+        Snacks.lazygit()
+      end,
+      desc = 'Lazygit',
+    },
+    {
+      '<leader>gl',
+      function()
+        Snacks.lazygit.log()
+      end,
+      desc = '[G]it [L]og',
+    },
+    {
+      '<c-/>',
+      function()
+        Snacks.terminal("zsh", {})
+      end,
+      desc = 'Terminal',
+    },
+    -- {
+    --   '<leader>N',
+    --   desc = 'Neovim News',
+    --   function()
+    --     Snacks.win {
+    --       file = vim.api.nvim_get_runtime_file('doc/news.txt', false)[1],
+    --       width = 0.6,
+    --       height = 0.6,
+    --       wo = {
+    --         spell = false,
+    --         wrap = false,
+    --         signcolumn = 'yes',
+    --         statuscolumn = ' ',
+    --         conceallevel = 3,
+    --       },
+    --     }
+    --   end,
+    -- },
+  },
 }
